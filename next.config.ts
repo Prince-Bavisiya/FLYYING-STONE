d76040341afda4977551ef07917c4410712b28df
Prince-Bavisiya <princebavisiya7397@gmail.com>
2026-08-07 18:14:34 +0530

    fix: adjust vercel routing priorities and conditional next.js rewrites to prevent 500 error

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
   },
   allowedDevOrigins: ['127.0.0.1', 'localhost', '192.168.1.2'],
   async rewrites() {
+    if (process.env.NODE_ENV === "production") {
+      return [];
+    }
     return [
       {
         source: "/api/:path*",
